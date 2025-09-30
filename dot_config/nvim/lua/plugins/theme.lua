acf1f4b541a8c845143354920416ec4d6d835c0d
Nabil Echaouch <echaouchna@gmail.com>
2025-09-30 23:08:23 +0200

    refactor(theme.lua): reorganize and enhance theme configurations for better structure and additional functionality

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,110 +1,134 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        opts = {
-            flavour = "frappe", -- latte, frappe, macchiato, mocha
-        },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
     },
-    -- {
-    --     "rose-pine/neovim",
-    --     name = "rose-pine",
-    --     opts = {
-    --         variant = "moon", -- auto, main, moon, or dawn,
-    --     },
-    -- },
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     lazy = false,
-    --     priority = 1000,
-    --     opts = {
-    --         style = "night", -- storm, moon, night, day
-    --     },
-    -- },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-        options = {
-            icons_enabled = true,
-            theme = 'auto',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-            disabled_filetypes = {
-                statusline = {},
-                winbar = {},
-            },
-            ignore_focus = {},
-            always_divide_middle = true,
-            globalstatus = false,
-            refresh = {
-                statusline = 1000,
-                tabline = 1000,
-                winbar = 1000,
-            },
-        },
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename' },
-            lualine_x = {
-                -- add pomo timer
-                function()
-                    local ok, pomo = pcall(require, "pomo")
-                    if not ok then
-                        return ""
-                    end
-
-                    local timer = pomo.get_first_to_finish()
-                    if timer == nil then
-                        return ""
-                    end
-
-                    return "󰄉 " .. tostring(timer)
-                end,
-                'encoding',
-                'fileformat',
-                'filetype',
-            },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' },
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
-            lualine_y = {},
-            lualine_z = {},
-        },
-        tabline = {},
+  },
+  -- {
+  --     "rose-pine/neovim",
+  --     name = "rose-pine",
+  --     opts = {
+  --         variant = "moon", -- auto, main, moon, or dawn,
+  --     },
+  -- },
+  -- {
+  --     "folke/tokyonight.nvim",
+  --     lazy = false,
+  --     priority = 1000,
+  --     opts = {
+  --         style = "night", -- storm, moon, night, day
+  --     },
+  -- },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local icons = {
+        Error = { " ", "DiagnosticError" },
+        Inactive = { " ", "MsgArea" },
+        Warning = { " ", "DiagnosticWarn" },
+        Normal = { LazyVim.config.icons.kinds.Copilot, "Special" },
+      }
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local status = require("sidekick.status").get()
+          return status and vim.tbl_get(icons, status.kind, 1)
+        end,
+        cond = function()
+          return require("sidekick.status").get() ~= nil
+        end,
+        color = function()
+          local status = require("sidekick.status").get()
+          local hl = status and (status.busy and "DiagnosticWarn" or vim.tbl_get(icons, status.kind, 2))
+          return { fg = Snacks.util.color(hl) }
+        end,
+      })
+    end,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    options = {
+      icons_enabled = true,
+      theme = "auto",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = {},
         winbar = {},
-        inactive_winbar = {},
-        extensions = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      },
     },
-    -- {
-    --     "folke/snacks.nvim",
-    --     opts = {
-    --         dashboard = {
-    --             enabled = true,
-    --             header = [[
-    --         ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-    --         ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-    --         ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-    --         ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-    --         ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-    --         ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-    --         }
-    --     }
-    -- },
-    { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
-    {
-        "nvimdev/dashboard-nvim",
-        lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
-        opts = function()
-            local logo = [[
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = {
+        -- add pomo timer
+        function()
+          local ok, pomo = pcall(require, "pomo")
+          if not ok then
+            return ""
+          end
+
+          local timer = pomo.get_first_to_finish()
+          if timer == nil then
+            return ""
+          end
+
+          return "󰄉 " .. tostring(timer)
+        end,
+        "encoding",
+        "fileformat",
+        "filetype",
+      },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
+  },
+  -- {
+  --     "folke/snacks.nvim",
+  --     opts = {
+  --         dashboard = {
+  --             enabled = true,
+  --             header = [[
+  --         ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+  --         ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+  --         ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+  --         ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+  --         ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+  --         ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+  --         }
+  --     }
+  -- },
+  { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
+  {
+    "nvimdev/dashboard-nvim",
+    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+    opts = function()
+      local logo = [[
 ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
 ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
 ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
@@ -113,17 +137,17 @@ return {
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
             ]]
 
-            logo = string.rep("\n", 8) .. logo .. "\n\n"
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-            local opts = {
-                theme = "doom",
-                hide = {
-                    -- this is taken care of by lualine
-                    -- enabling this messes up the actual laststatus setting after loading a file
-                    statusline = false,
-                },
-                config = {
-                    header = vim.split(logo, "\n"),
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
                     -- stylua: ignore
                     center = {
                         { action = 'lua LazyVim.pick()()', desc = " Find File", icon = " ", key = "f" },
@@ -136,33 +160,33 @@ return {
                         { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
                         { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit", icon = " ", key = "q" },
                     },
-                    footer = function()
-                        local stats = require("lazy").stats()
-                        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-                        return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-                    end,
-                },
-            }
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
 
-            for _, button in ipairs(opts.config.center) do
-                button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-                button.key_format = "  %s"
-            end
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
 
-            -- open dashboard after closing lazy
-            if vim.o.filetype == "lazy" then
-                vim.api.nvim_create_autocmd("WinClosed", {
-                    pattern = tostring(vim.api.nvim_get_current_win()),
-                    once = true,
-                    callback = function()
-                        vim.schedule(function()
-                            vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
-                        end)
-                    end,
-                })
-            end
+      -- open dashboard after closing lazy
+      if vim.o.filetype == "lazy" then
+        vim.api.nvim_create_autocmd("WinClosed", {
+          pattern = tostring(vim.api.nvim_get_current_win()),
+          once = true,
+          callback = function()
+            vim.schedule(function()
+              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+            end)
+          end,
+        })
+      end
 
-            return opts
-        end,
-    },
+      return opts
+    end,
+  },
 }
